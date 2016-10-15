0ba80b260f3588a9d87316123032e41ce9539908
nikolalsvk <nikolaseap@gmail.com>
2016-10-15 19:13:24 +0200

    Add mBills.js

---
+++ mBills.js
@@ -0,0 +1,49 @@
+var request = require('request');
+var sha256 = require('sha256');
+
+var requestURL = 'https://hack.halcom.com/MBillsWS/API/v1/transaction/sale';
+var secretKey = '1'
+
+var username = `03ec7443-071c-4668-b310-c3b81e78d0eb.12345678.${Math.floor(new Date() / 1000)}`;
+var password = sha256(username + secretKey + requestURL);
+
+console.log(username);
+console.log(password);
+
+var pay = function(amount) {
+
+  request({
+    method: 'POST',
+    url: requestURL,
+    auth: {
+      user: username,
+      pass: password
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: `{ \"amount\": ${amount},
+             \"currency\": \"EUR\",
+             \"purpose\": \"Online payment\",
+             \"paymentreference\": \"SI0011072015\",
+             \"orderid\": \"124134987\",
+             \"channelid\": \"eshop1\" }`
+  }, function(error, response, body) {
+    console.log('Status:', response.statusCode);
+    console.log('Headers:', JSON.stringify(response.headers));
+    console.log('Response:', body);
+
+    return response, body;
+  });
+
+};
+
+var invoice = function() {
+
+}
+
+result = pay(100);
+
+console.log(result);
+
+module.export = pay
